ca1d6cf1a0df098f9d9ecced66eb7a0b5c1497c2
boenfu <1997@boenfu.cn>
2019-03-17 17:07:49 +0800

    Fix publicPath

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,7 +1,7 @@
 const production = process.env.NODE_ENV === "production";
 
 module.exports = {
-  publicPath: production ? "/demo/dist" : "",
+  publicPath: production ? "/vuex-along/demo/dist" : "",
   chainWebpack: config => {
     config.plugin("html").tap(args => {
       production ? (args[0].filename = "../../index.html") : undefined;
