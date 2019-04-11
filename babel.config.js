6f275fc66f74056e33c41d9a5d78ed2fd0f26d5f
boenfu <1997@boenfu.cn>
2019-04-12 02:16:46 +0800

    Add babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   presets: ["@babel/typescript"],
   plugins: [
-    "@babel/plugin-transform-block-scoping",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-transform-classes"
+    ["@babel/plugin-proposal-class-properties", { loose: false }],
+    "@babel/plugin-transform-classes",
+    "@babel/plugin-transform-parameters",
+    "@babel/plugin-transform-arrow-functions",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-block-scoping"
   ]
 };
